6d25b7aa4161a8e83329dfab6330efafd344499f
Jose Juan Pena <josejuan-98@hotmail.com>
2023-05-08 18:37:37 +0200

    feature: python library

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -13,19 +13,22 @@ src_path = os.path.join(parent_path, "src")
 
 def remove(filepath):
     if os.path.isfile(filepath):
-        os.remove(filepath)
+        os.remove(path=filepath)
     elif os.path.isdir(filepath):
-        shutil.rmtree(filepath)
+        shutil.rmtree(path=filepath)
 
 
 if "{{cookiecutter.project_kind}}" == "python library":
     # User does not want folder so remove it
-    [remove(folder) for
-     folder in os.listdir(src_path)
-     if folder not in ["{{ cookiecutter.project_name }}", "__init__.py", "py.typed"]
-     ]
-    remove(os.path.join(parent_path, "requirements", "test.txt"))
-    remove(os.path.join(parent_path, "requirements", "analysis.txt"))
+    for folder in os.listdir(src_path):
+        if folder not in ["{{ cookiecutter.project_name }}", "__init__.py", "py.typed"]:
+            remove(os.path.join(src_path, folder))
+
+    for file in ["test.txt", "analysis.txt"]:
+        remove(os.path.join(parent_path, "requirements", file))
+
+    for folder in ["artifacts", "notebooks", "reports"]:
+        remove(os.path.join(parent_path, folder))
 
 else:
     remove(os.path.join(src_path, "{{ cookiecutter.project_name }}"))
